3303d09bf5261487502f77a5dce0a16aba95d32c
obwan02 <obwan02@hotmail.co.nz>
2023-03-26 00:30:29 +1300

    update: Trying to fix powershell bugs

--- install_tools_win.ps1
+++ install_tools_win.ps1
@@ -29,11 +29,11 @@ if( !(Test-Path -Path "$HOME\Programs" -PathType Container) ) {
 }
 
 try {
-	Expand-Archive -Force -Path $GhdlTemp -DestinationPath "$HOME\Programs"
+	Expand-Archive -Force -LiteralPath $GhdlTemp -DestinationPath "$HOME\Programs"
 	Write-Host "[+] Extracted GHDL to $HOME\Programs"
-	Expand-Archive -Force -Path $JustTemp -DestinationPath "$HOME\Programs\just"
+	Expand-Archive -Force -LiteralPath $JustTemp -DestinationPath "$HOME\Programs\just"
 	Write-Host "[+] Extracted just to $HOME\Programs"
-	Expand-Archive -Force -Path $GtkWaveTemp -DestinationPath "$HOME\Programs"
+	Expand-Archive -Force -LiteralPath $GtkWaveTemp -DestinationPath "$HOME\Programs"
 	Write-Host "[+] Extracted GTKWave to $HOME\Programs"
 } catch {
 	Write-Host "[x] Failed to extract one or more archives. exiting ..."
@@ -55,13 +55,14 @@ $ProfileContent = Get-Content $PROFILE.CurrentUserAllHosts
 
 Write-Host "[-] Adding programs to PATH. This is done through modifying the current user's powershell profile. See https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.3 for more information."
 
+Write-Host ""
 # Check if binaries are already on path, nd if not, add them
 Write-Host "> Checking if GHDL is in PATH"
 if( !(Get-Command ghdl -ErrorAction SilentlyContinue) ) {
 	Write-Host " - Could not find GHDL in PATH"
 	Write-Host " + Modifying profile to add GHDL to PATH"
 
-	Write-Host "`$env:PATH = `$env:PATH + ';' + '$HOME\Programs\GHDL\bin'" | Add-Content $PROFILE.CurrentUserAllHosts -Encoding UTF8
+	echo "`$env:PATH = `$env:PATH + ';' + '$HOME\Programs\GHDL\bin'" | Add-Content $PROFILE.CurrentUserAllHosts -Encoding UTF8
 } else {
 	Write-Host " + Found GHDL binary in PATH @ $((Get-Command ghdl).Path)"
 }
@@ -71,7 +72,7 @@ if( !(Get-Command gtkwave -ErrorAction SilentlyContinue) ) {
 	Write-Host " - Could not find just in PATH"
 	Write-Host " + Modifying profile to add just to PATH"
 
-	Write-Host "`$env:PATH = `$env:PATH + ';' + '$HOME\Programs\just'" | Add-Content $PROFILE.CurrentUserAllHosts -Encoding UTF8
+	echo "`$env:PATH = `$env:PATH + ';' + '$HOME\Programs\just'" | Add-Content $PROFILE.CurrentUserAllHosts -Encoding UTF8
 } else {
 	Write-Host " + Found just binary in PATH @ $((Get-Command just).Path)"
 }
@@ -81,7 +82,7 @@ if( !(Get-Command gtkwave -ErrorAction SilentlyContinue) ) {
 	Write-Host " - Could not find GTKWave in PATH"
 	Write-Host " + Modifying profile to add GTKWave to PATH"
 
-	Write-Host "`$env:PATH = `$env:PATH + ';' + '$HOME\Programs\GTKWave'" | Add-Content $PROFILE.CurrentUserAllHosts -Encoding UTF8
+	echo "`$env:PATH = `$env:PATH + ';' + '$HOME\Programs\GTKWave'" | Add-Content $PROFILE.CurrentUserAllHosts -Encoding UTF8
 } else {
 	Write-Host " + Found GTKWave binary in PATH @ $((Get-Command gtkwave).Path)"
 }
